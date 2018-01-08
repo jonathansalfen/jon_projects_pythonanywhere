61f42827bf9a7a78fcd03e50fb8fb605b444fb58
Jonathan Salfen <jonathan.salfen@gmail.com>
2018-01-08 14:23:36 -0800

    added form requirements and edited instructions line on story_intro

--- jon_projects/settings.py
+++ jon_projects/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+l#-x+_(9e&_1!cbf3c*#of#a5n#*^2mbrfnto5xxlb&s4sd6q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["jonathansalfen.pythonanywhere.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,4 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
